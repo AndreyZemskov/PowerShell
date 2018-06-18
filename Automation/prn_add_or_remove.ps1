d9850f2d1c7ea35fcf03dce1df47fa9331830e01
Biggie <sm1le.cfg@gmail.com>
2018-06-18 14:08:52 +0300

    Installing a network printer on a remote PC

---
+++ Automation/prn_add_or_remove.ps1
@@ -0,0 +1,13 @@
+﻿$ComputerName = "NamePC"
+$NetPrinter = "`"\\path\name_printer`""
+foreach ($printers in $ComputerName)
+{
+# Installing a network printer on a remote PC
+iex "RUNDLL32 PRINTUI.DLL,PrintUIEntry /ga /c\\$printers /n$NetPrinter"
+}
+
+# # Delete the network printer on a remote PC
+# iex "RUNDLL32 PRINTUI.DLL PrintUIEntry /gd /c\\$ComputerName /n$NetPrinter Gw /q"
+ 
+# # Get a list of network printers from a remote PC
+# icm -Comp $ComputerName -ScriptBlock { ls "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Print\Connections" | %{$_.GetValue("Printer")} }
