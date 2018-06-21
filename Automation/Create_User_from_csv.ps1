350774567349f62fc147c839ffbe3ed692b7b251
Biggie <sm1le.cfg@gmail.com>
2018-06-21 12:16:11 +0300

    Creating users from CSV file

---
+++ Automation/Create_User_from_csv.ps1
@@ -0,0 +1,3 @@
+﻿$users = Import-Csv C:\Users\Administrator\Desktop\users2.csv
+$password = Read-Host -AsSecureString
+foreach ($user in $users) {New-ADUser $user.name -EmailAddress.email -AccountPassword $password -Enabled $true}
