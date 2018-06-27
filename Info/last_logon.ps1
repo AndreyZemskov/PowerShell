3428a00a0b71ce6ed2ce4552e9f5dac722a0d5be
Biggie <sm1le.cfg@gmail.com>
2018-06-27 09:36:08 +0300

    Last logon

---
+++ Info/last_logon.ps1
@@ -0,0 +1,10 @@
+﻿Get-ADComputer -Identity UserName
+
+# A list PC to last logon
+# Get-ADComputer -Filter * -Properties * | FT Name, LastLogonDate -Autosize | Export-CSV D:\last_logon.csv -NoTypeInformation -Encoding utf8
+
+# Ctreate a variable with a date 
+# $date_with_offset= (Get-Date).AddDays(-30)
+
+# Filter by date
+# Get-ADComputer  -Properties LastLogonDate -Filter {LastLogonDate -lt $date_with_offset } | Sort LastLogonDate | FT Name, LastLogonDate -Autosize
