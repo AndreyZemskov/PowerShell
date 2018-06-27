a16f7ed5b467449d7c4ebe7ec709d4545ddfcbff
Biggie <sm1le.cfg@gmail.com>
2018-06-27 09:44:23 +0300

    Information about users and passwords

---
+++ Info/users_and_ passwords.ps1
@@ -0,0 +1,11 @@
+﻿# Password change information
+# Get-ADUser UserName -properties PasswordExpired, PasswordLastSet, PasswordNeverExpires 
+
+# All accounts
+# Get-ADUser -Filter {SamAccountName -like "*"} | Measure-Object
+ 
+# Active accounts in AD
+# Get-ADUser -Filter {Enabled -eq "True"} | Select-Object SamAccountName,Name,Surname,GivenName | Format-Table  # | Export-Csv -NoTypeInformation -Encoding utf8 -delimiter "," D:\export_ad_list.csv
+
+# List of accounts with expired password
+# Get-ADUser -filter {Enabled -eq $True} -properties passwordExpired | where {$_.PasswordExpired}
